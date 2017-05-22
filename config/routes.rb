d3e8308ea763c3dbafaf72f77eb8b776e28249e6
Herman Hui <hshui@sfu.ca>
2017-05-21 17:17:34 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  #routes a get request for '/help' to: 'help' action in static pages controller
+  #each rule will create two routes help_path -> '/help' and help_url -> 'http:www.example.com/help' (full url)
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
